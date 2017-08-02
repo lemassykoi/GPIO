741652a45db30432244513a90d646861f0c33fd1
Clément PAPPALARDO <16377344+lemassykoi@users.noreply.github.com>
2017-08-02 20:59:00 +0200

    Update action.php

--- action.php
+++ action.php
@@ -5,13 +5,13 @@
 </head>
 <body>
 <?php
-$arr = array(0, 5, 6, 13, 26, 21, 25, 16, 20);  // Les GPIO que j'utilise, et zero en plus car le comptage commence à zero
+$arr = array(0, 5, 6, 13, 26, 21, 25, 16, 20);  // Here are the GPIO I use. Change to yours.
 if(isset($_GET["pin"])) {
-                $script = shell_exec('sudo /home/pi/gpio.sh ' . $_GET["pin"] . ' ' . $_GET["state"]);
-                echo 'PIN' . $_GET["pin"] . ' is set to ' . $_GET["state"] . '.<br/>';
-                echo 'GPIO is set to : ' . $arr[$_GET["pin"]] . '.<br/>';
+  $script = shell_exec('sudo /home/pi/gpio.sh ' . $_GET["pin"] . ' ' . $_GET["state"]);
+  echo 'PIN' . $_GET["pin"] . ' is set to ' . $_GET["state"] . '.<br/>'; // this is for when you manually call action.php to debug
+  echo 'GPIO is set to : ' . $arr[$_GET["pin"]] . '.<br/>'; // this is for when you manually call action.php to debug
 }
-else { echo 'ERROR<br/> Aucune Valeur de GET !'; }
+  else { echo 'ERROR<br/> No GET Value !'; }
 ?>
 </body>
 </html>
